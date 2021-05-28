24fbaa463b4bf683c38b0ce6d4d6b4421fd87242
Cong Minh Vuong <cminh.v566@gmail.com>
2021-05-29 05:17:59 +0700

    Create debug.php

---
+++ debug.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>
+      Current IP Check
+    </title>
+  </head>
+  <body>
+    REMOTE_ADDR IP Address: <?=$_SERVER['REMOTE_ADDR']?>
+    </br>
+    HTTP_X_FORWARDED_FOR IP Address: <?=$_SERVER['HTTP_X_FORWARDED_FOR']?>
+  </body>
+</html>
